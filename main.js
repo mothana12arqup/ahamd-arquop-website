cd54ebf1dacc679ca02c2240fb3838c41542c126
mothana12arqup <22418015@students.hebron.edu>
2026-06-21 03:30:08 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+const menuBtn = document.getElementById('menuBtn');
+const navLinks = document.getElementById('navLinks');
+if (menuBtn && navLinks) {
+  menuBtn.addEventListener('click', () => navLinks.classList.toggle('show'));
+}
